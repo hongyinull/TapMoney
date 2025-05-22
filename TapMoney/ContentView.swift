8cce61e335e1cda69074394b80eb679505657c1c
hongyinull <80037820+hongyinull@users.noreply.github.com>
2025-05-23 04:47:06 +0800

    Initial Commit

---
+++ TapMoney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapMoney
+//
+//  Created by HONGYINULL on 2025/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapMoney/TapMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapMoneyApp.swift
+//  TapMoney
+//
+//  Created by HONGYINULL on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TapMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
